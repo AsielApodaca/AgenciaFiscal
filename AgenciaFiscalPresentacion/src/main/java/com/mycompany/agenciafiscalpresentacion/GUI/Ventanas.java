b6d14b7246592e764edc66457d7924e2775a7b2c
luisa <moraLF20@outlook.com>
2024-03-22 14:17:04 -0700

    funcion para cerrar las conexiones
    puse un windowListener para que cierre las conexiones cuando se cierra la ventana. Se cierran con el metodo cerrarConexiones de la clase registrarLicenciaBO

--- AgenciaFiscalPresentacion/src/main/java/com/mycompany/agenciafiscalpresentacion/GUI/Ventanas.java
+++ AgenciaFiscalPresentacion/src/main/java/com/mycompany/agenciafiscalpresentacion/GUI/Ventanas.java
@@ -2,19 +2,30 @@ package com.mycompany.agenciafiscalpresentacion.GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import iBo.iRegistrarLicenciaBO;
 
 public class Ventanas extends JFrame {
 
     private CardLayout cardLayout;
     private JPanel cardPanel;
-
+    static iRegistrarLicenciaBO registrar;
     public Ventanas() {
         setTitle("Agencia fiscal");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(640 + getInsets().top, 360);
         setLocationRelativeTo(null);
         setResizable(false);
-
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                // esto es para cerrar las conexiones con la bd cuando se cierre el programa
+                if(registrar!=null)
+                    registrar.cerrarConexiones();
+            }
+        });
+        
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
 
@@ -30,6 +41,10 @@ public class Ventanas extends JFrame {
         cardLayout.show(cardPanel, nombrePanel);
     }
 
+    public static void setRegistrarLicenciaBO(iRegistrarLicenciaBO rlb){
+        registrar=rlb;
+    }
+    
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
