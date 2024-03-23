05456d1423562ec52d2f693833b691664d91e067
luisa <moraLF20@outlook.com>
2024-03-22 23:49:32 -0700

    Update Ventanas.java

--- AgenciaFiscalPresentacion/src/main/java/com/mycompany/agenciafiscalpresentacion/GUI/Ventanas.java
+++ AgenciaFiscalPresentacion/src/main/java/com/mycompany/agenciafiscalpresentacion/GUI/Ventanas.java
@@ -1,5 +1,6 @@
 package com.mycompany.agenciafiscalpresentacion.GUI;
 
+import bo.RegistrarLicenciaBO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
@@ -10,13 +11,17 @@ public class Ventanas extends JFrame {
 
     private CardLayout cardLayout;
     private JPanel cardPanel;
-    static iRegistrarLicenciaBO registrar;
+    protected static iRegistrarLicenciaBO registrar;
+    
     public Ventanas() {
         setTitle("Agencia fiscal");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(640, 360);
         setLocationRelativeTo(null);
         setResizable(false);
+        
+        registrar=new RegistrarLicenciaBO();
+        
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -41,10 +46,9 @@ public class Ventanas extends JFrame {
         cardLayout.show(cardPanel, nombrePanel);
     }
 
-    public static void setRegistrarLicenciaBO(iRegistrarLicenciaBO rlb){
-        registrar=rlb;
+    public RegistrarLicenciaBO getBO(){
+        return (RegistrarLicenciaBO)registrar;
     }
-    
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
