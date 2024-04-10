1eb83fc0c5194c5905e5835605cb0806db720cac
Gh0zTy1 <160164643+Gh0zTy1@users.noreply.github.com>
2024-04-09 21:15:19 -0700

    dardebajalaslicencias

--- AgenciaFiscalNegocio/src/main/java/bo/BajaLicencia.java
+++ AgenciaFiscalNegocio/src/main/java/bo/BajaLicencia.java
@@ -4,10 +4,64 @@
  */
 package bo;
 
-/**
- *
- * @author caarl
- */
-public class BajaLicencia {
-    
+import daos.TramiteLicenciaDAO;
+import daos.TramiteLicenciaDAO;
+import entidades.TramiteLicencia;
+import entidades.Estado;
+import excepciones.NegocioException;
+import excepciones.PersistenciaException;
+import negocioDTO.TramiteLicenciaDTO;
+import negocioDTO.PersonaDTO;
+import negocioDTO.EstadoDTO;
+import iBo.IBajaLicencia;
+
+
+public class BajaLicencia implements IBajaLicencia {
+    private static TramiteLicenciaDAO TramiteLicenciaDao;
+
+    public BajaLicencia() {
+        BajaLicencia.TramiteLicenciaDao = new TramiteLicenciaDAO();
+    }
+
+    @Override
+    public TramiteLicenciaDTO obtenerLicencia(TramiteLicenciaDTO licencia) throws NegocioException {
+        TramiteLicencia licenciaConsultada;
+        try {
+            licenciaConsultada = new TramiteLicencia();
+            licenciaConsultada.setNumLicencia(licencia.getNumLicencia());
+            licenciaConsultada = TramiteLicenciaDao.obtenerLicenciaVigente(licenciaConsultada);
+        } catch (PersistenciaException pe) {
+            throw new NegocioException(pe.getMessage());
+        }
+
+        TramiteLicenciaDTO licenciaDTO = new TramiteLicenciaDTO();
+        PersonaDTO persona = new PersonaDTO();
+        EstadoDTO estado;
+
+        estado = switch (licenciaConsultada.getEstado().toString()) {
+            case "VIGENTE" -> EstadoDTO.VIGENTE;
+            case "CADUCO" -> EstadoDTO.CADUCO;
+            default -> null;
+        };
+
+        persona.setNombreCompleto(licenciaConsultada.getPersona().getNombreCompleto());
+        licenciaDTO.setNumLicencia(licenciaConsultada.getNumLicencia());
+        licenciaDTO.setEstado(estado);
+        licenciaDTO.setPersona(persona);
+
+        return licenciaDTO;
+    }
+
+    @Override
+    public boolean darDeBajaLicencia(TramiteLicenciaDTO licencia) throws NegocioException {
+        try {
+            TramiteLicencia licenciaADarDeBaja = new TramiteLicencia();
+            licenciaADarDeBaja.setNumLicencia(licencia.getNumLicencia());
+            licenciaADarDeBaja.setEstado(Estado.CADUCO);
+            TramiteLicenciaDao.actualizarEstadoTramite(licenciaADarDeBaja);
+            return true;
+        } catch (PersistenciaException pe) {
+            throw new NegocioException(pe.getMessage());
+        }
+    }
 }
--- AgenciaFiscalNegocio/src/main/java/iBo/IBajaLicencia.java
+++ AgenciaFiscalNegocio/src/main/java/iBo/IBajaLicencia.java
@@ -4,10 +4,10 @@
  */
 package iBo;
 
-/**
- *
- * @author caarl
- */
+import negocioDTO.TramiteLicenciaDTO;
+import excepciones.NegocioException;
+
 public interface IBajaLicencia {
-    
+    TramiteLicenciaDTO obtenerLicencia(TramiteLicenciaDTO licencia) throws NegocioException;
+    boolean darDeBajaLicencia(TramiteLicenciaDTO licencia) throws NegocioException;
 }
