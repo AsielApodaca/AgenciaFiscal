ec36a1cd27ff94f7a1f708c90985d70795a9df0e
Asiel Apodaca Monge <asiel_apodaca@hotmail.com>
2024-04-12 00:12:51 -0700

    Corrección de método

--- AgenciaFiscalNegocio/src/main/java/bo/GenerarReporteBO.java
+++ AgenciaFiscalNegocio/src/main/java/bo/GenerarReporteBO.java
@@ -6,14 +6,16 @@ import negocioDTO.PersonaDTO;
 import entidades.Persona;
 import entidades.Tramite;
 import entidades.Estado;
+import entidades.TramiteLicencia;
 import excepciones.NegocioException;
 import excepciones.PersistenciaException;
 import iBo.IGenerarReporteBO;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import negocioDTO.EstadoDTO;
 import negocioDTO.TramiteDTO;
+import negocioDTO.TramiteLicenciaDTO;
+import negocioDTO.TramitePlacasDTO;
 
 /**
  *
@@ -30,43 +32,30 @@ public class GenerarReporteBO implements IGenerarReporteBO{
     @Override
     public List<TramiteDTO> obtenerTramites(Calendar fechaDesde, Calendar fechaHasta, String tipoTramite, PersonaDTO personaTramiteDTO) throws NegocioException {
         try {
-            Persona personaTramite = new Persona();
-            if(personaTramiteDTO != null)
-                personaTramite.setRfc(personaTramiteDTO.getRfc());
+            Persona personaTramite = null;
+            if(personaTramiteDTO != null){
+                personaTramite = new Persona(personaTramiteDTO.getRfc());
+            }
             List<Tramite> tramites = tramiteDAO.obtenerTramites(fechaDesde, fechaHasta, tipoTramite, personaTramite);
             
             List<TramiteDTO> tramitesDTO = new ArrayList<>();
             
             // Entidad tramite a Dto tramite
-            for(Tramite tramite : tramites) {
-                
-                
-                TramiteDTO tramiteDTO = new TramiteDTO() {};
-                
-                PersonaDTO personaDTO = new PersonaDTO();
-                Persona persona = new Persona();
-                personaDTO.setRfc(persona.getRfc());
-                personaDTO.setNombreCompleto(persona.getNombreCompleto());
-                ;
-                EstadoDTO estadoDTO = null;
-                switch(tramite.getEstado().toString()) {
-                    case "ACTIVO":
-                        estadoDTO = EstadoDTO.ACTIVO;
-                    case "CADUCO":
-                        estadoDTO = EstadoDTO.CADUCO;
-                        break;
-                    case "INACTIVO":
-                        estadoDTO = EstadoDTO.INACTIVO;
-                        break;
-                    case "VIGENTE":
-                        estadoDTO = EstadoDTO.VIGENTE;
-                        break;
-                    default:
-                        break;
+            for (Tramite tramite : tramites) {
+                TramiteDTO tramiteDTO;
+                if (tramite instanceof TramiteLicencia) {
+                    tramiteDTO = new TramiteLicenciaDTO();
+                } else {
+                    tramiteDTO = new TramitePlacasDTO();
                 }
                 
+                PersonaDTO personaDTO = new PersonaDTO();
+                Persona persona = tramite.getPersona();
+                personaDTO.setRfc(persona.getRfc());
+                personaDTO.setNombreCompleto(persona.getNombreCompleto());
+                
+                
                 tramiteDTO.setPersona(personaDTO);
-                tramiteDTO.setEstado(estadoDTO);
                 tramiteDTO.setFechaEmision(tramite.getFechaEmision());
                 tramiteDTO.setCostoMxn(tramite.getCostoMxn());
                 
