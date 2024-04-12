c32a7995e767f0476bac1bfda42929f461495311
luisa <moraLF20@outlook.com>
2024-04-11 21:44:45 -0700

    cambiando consultasBO
    cambie el metodo para que se pueda especificar que tipo de criterio (nombre, curp, fecha nac) se usara para la consulta

--- AgenciaFiscalNegocio/src/main/java/iBo/IConsultasBO.java
+++ AgenciaFiscalNegocio/src/main/java/iBo/IConsultasBO.java
@@ -7,11 +7,13 @@ package iBo;
 import excepciones.NegocioException;
 import java.util.List;
 import negocioDTO.PersonaDTO;
+import negocioDTO.TramiteDTO;
 
 /**
  *
  * @author luiis
  */
 public interface IConsultasBO {
-    public List<PersonaDTO> consultarPersonasPorNombre(PersonaDTO persona)throws NegocioException;
+    public List<PersonaDTO> consultarPersonasPorCriterio(PersonaDTO persona, String criterioBusqueda)throws NegocioException;
+    public List<TramiteDTO> consultarTramitesPorPersona(PersonaDTO persona)throws NegocioException;
 }
